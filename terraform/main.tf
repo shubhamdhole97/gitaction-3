fb7f4a5d90dc800a977f2a44f46dc3f7f16f561e
shubhamdhole97 <shubhamdhole97@gmail.com>
2025-08-08 11:03:01 +0530

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,72 @@
+variable "ssh_user" {
+  type = string
+}
+
+variable "ssh_pub_key" {
+  type = string
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "shubham-project-468314"
+  region      = "us-central1"
+  zone        = "us-central1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "small-server"
+  machine_type = "e2-small"
+  zone         = "us-central1-a"
+  tags         = ["ssh-access"]  # tag used for firewall targeting
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${var.ssh_pub_key}"
+
+    startup-script = <<-EOF
+      #!/bin/bash
+      PORTS="22 2221 2222 2223 2224 2225"
+      for PORT in $PORTS; do
+        grep -q "^Port $PORT" /etc/ssh/sshd_config || echo "Port $PORT" >> /etc/ssh/sshd_config
+      done
+      systemctl restart sshd
+    EOF
+  }
+}
+
+# ✅ Firewall rule to allow SSH on multiple ports
+resource "google_compute_firewall" "allow_custom_ssh" {
+  name    = "allow-custom-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "2221", "2222", "2223", "2224", "2225"]
+  }
+
+  direction     = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh-access"]
+}
+
+output "vm_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
