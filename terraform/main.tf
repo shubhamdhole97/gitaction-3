d9785fb8781bec5aadd30fe3f2c0c79f523f3bc4
shubhamdhole97 <shubhamdhole97@gmail.com>
2025-08-08 17:44:20 +0530

    25

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "small-searvsser"
+  name         = "small-searver"
   machine_type = "e2-small"
   zone         = "us-central1-a"
   tags         = ["ssh-access"]  # tag used for firewall targeting
